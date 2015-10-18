7c9bf663aa4b171f3974cbd6fac3ff5d40a2da95
vycb <vycb777@gmail.com>
2015-10-18 07:50:28 +0300

    redis dump

---
+++ export.js
@@ -0,0 +1,23 @@
+var dump = require('redis-dump-tool'),
+redis = require("redis"),
+client = redis.createClient("redis://localhost",{parser:"hiredis"})
+//		client:"redis://rediscloud:jVuF0mshqeDmSSxc@pub-redis-13088.us-east-1-4.6.ec2.redislabs.com:13088"*/
+;
+
+dump({
+	filter: 'Homo*',
+	count:	10000,
+	client:	client,
+	db:	1
+	},
+	function(err, data){
+		'use strict';
+
+		if (err) {
+			console.log(err);
+			return;
+		}
+
+		console.log(data);
+	}
+);
