4db1f7e7fc9b318cad5278bdbc1dd515612ed8dd
vycb <vycb777@gmail.com>
2015-12-09 00:29:06 +0200

    new ejs

--- tol.js
+++ tol.js
@@ -3,12 +3,16 @@
 /* global local, end, cjson, out, script */
 
 var redis = require("redis"),
-	/*		client = redis.createClient("redis://vycb777@gmail.com:1qaz2wsx@pub-redis-11548.us-east-1-3.2.ec2.garantiadata.com:11548"),
-	 client = redis.createClient("redis://rediscloud:jVuF0mshqeDmSSxc@pub-redis-13088.us-east-1-4.6.ec2.redislabs.com:13088",{parser:"hiredis"}),*/
-	client=redis.createClient("redis://localhost", {parser: "hiredis"})
+
+	client = redis.createClient("redis://vycb777@gmail.com:1qaz2wsx@pub-redis-11548.us-east-1-3.2.ec2.garantiadata.com:11548")
+	/*
+	 * client = redis.createClient("redis://rediscloud:jVuF0mshqeDmSSxc@pub-redis-13088.us-east-1-4.6.ec2.redislabs.com:13088",{parser:"hiredis"})
+	 *
+	 * client=redis.createClient("redis://localhost", {parser: "hiredis"})*/
 	;
-client.select(1);
+client.select(0);
 exports.childs = function(sq, callback){
+
 	if(!sq) sq="Life on Earth:1"
 
 	var script=`
@@ -50,12 +54,14 @@ exports.childs = function(sq, callback){
     cjson.encode_sparse_array(true)
     return cjson.encode(out)
 `;
-
+/*
 	client.eval(script, [0, sq], function(err, res){
 		if(err)
 			console.error(err)
-		callback(err, {r: JSON.parse(res), sq: sq})
-	})
+		callback(err, {r: JSON.parse(res||{}), sq: sq})
+	})*/
+
+	callback(null, {r:{}, ch:{}, sq:sq})
 }
 exports.paging=function(q, callback){
 	if(!q.sq)q.sq = "*"
